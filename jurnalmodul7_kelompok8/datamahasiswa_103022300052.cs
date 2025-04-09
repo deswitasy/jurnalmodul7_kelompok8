f88becc60833d81e1a543926fcab9157440aff76
apprliaaaaaa <liania470@gmail.com>
2025-04-09 08:06:11 +0700

    menambahkan datamahasiswa

---
+++ jurnalmodul7_kelompok8/datamahasiswa_103022300052.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace jurnalmodul7_kelompok8
+{
+    class datamahasiswa_103022300052
+    {
+        public void ReadJSON()
+        {
+            string json = File.ReadAllText("jurnal7_1_103022300052.json");
+
+            mahasiswa_103022300052 data = JsonSerializer.Deserialize<mahasiswa_103022300052>(json);
+            Console.WriteLine($"Nama {data.firstName} {data.lastName}" +
+                $"umur dan gender  {data.gender} {data.age} + " +
+                $"alamat {data.address.state} {data.address.city}");
+            foreach (var course in data.courses)
+            {
+                Console.WriteLine($"Mata Kuliah {course.code} {course.name}");
+            }
+
+        }
+    }
+}
